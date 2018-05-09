cde4779999f402bb6739a621529653736af32a61
Sanket Chirame <sanketchirame12@gmail.com>
2018-05-09 16:56:15 +0530

    added QA and test doc report

--- constants_1U.py
+++ constants_1U.py
@@ -1,12 +1,16 @@
 #Constants used in the code for simulation of 1U satellite
 
 import numpy as np
-
+import datetime as dt
+from math import sqrt
 W_EARTH = 7.2921150e-5; # rotation velocity of the earth (rad per second)
 G = 6.67408e-11; #universal gravitational constant, SI
 M = 5.972e24; #mass of earth, kg
 R = 6371.0e3; #radius of earth, m
-
+#date format yyyy,mm,dd
+LAUNCHDAY = dt.datetime(2018,5,1)	#date of launch t=0
+EQUINOX = dt.datetime(2018,3,20)	#day of equinox
+steprut = 1.002738 #siderial time = stperut * universal time
 #-- Moment of inertia matrix in kgm^2 for 1U satellite (assumed to be uniform with small off-diagonal)
 Ixx = 0.0015
 Iyy = 0.0015
@@ -19,15 +23,15 @@ m_INERTIA = np.array([[Ixx, Ixy, Ixz], [Ixy, Iyy, Iyz], [Ixz, Iyz, Izz]])
 m_INERTIA_inv = np.linalg.inv(m_INERTIA)	#inverse of inertia matrix
 
 v_q0_BO = np.array([1.,0.,0.,0.])	#unit quaternion initial condition
-v_w0_BOB = np.array([0.,-1*math.sqrt(G*M/R**3),0.])	#initial angular velocity
-v_STATE0 = np.hstack((q0,w0))	#Initial state vector [q_BO , w_BOB]
+v_w0_BOB = np.array([0.,-1*sqrt(G*M/R**3),0.])	#initial angular velocity
+v_STATE0 = np.hstack((v_q0_BO,v_w0_BOB))	#Initial state vector [q_BO , w_BOB]
 
 
 CONTROL_STEP = 2.0	#control cycle time period in second
 FREQ = 1e3 	#frequency of duty cycle in Hz
 
-INDUCTANCE = 1e-3	#Inductance of torquer
-RESISTANCE = 100.0	#Resistance of torquer
+INDUCTANCE = 1e-3	#Inductance of torquer in Henry
+RESISTANCE = 100.0	#Resistance of torquer	in Ohm
 
 v_Ax = np.array([0.01,0.,0.])	#area vector perpendicular to x-axis in m^2
 v_Ay = np.array([0.,0.01,0.])	#area vector perpendicular to y-axis in m^2
--- frames.py
+++ frames.py
@@ -1,21 +1,26 @@
+#Code for transformation of vector from one reference frame to another
+
 import numpy as np
-from constants import *
-from math import radians, sin, cos, asin, acos, pi
+from constants_1U import W_EARTH, LAUNCHDAY, EQUINOX
+from math import radians, sin, cos, acos, pi
 
 
 def latlon(x):
-	#get the latitude and longitude given position in ECEF
-	lat=sgn(x[2])*(acos(((x[0]**2+x[1]**2)**0.5)/((x[0]**2+x[1]**2+x[2]**2)**0.5)))*90/(pi/2) 
-        
+	#get the latitude and longitude in degrees given position in ECEF 
+	#x is position in ecif
+	#latitude ranges [0,90] in north hemisphere and [0,-90] in south hemisphere
+	lat=sgn(x[2])*(acos(((x[0]**2+x[1]**2)**0.5)/((x[0]**2+x[1]**2+x[2]**2)**0.5)))*90./(pi/2.) 
+       
 	# longitude calculation given position, lon is longitude
+	#ranges from (-pi,pi]
 	if x[1]==0:
 		if x[0]>=0 :
 			lon = 0.0
 		else:
-			lon = pi
+			lon = 180.0
 
 	else:
-		lon=sgn(x[1])*acos(x[0]/((x[0]**2+x[1]**2)**0.5))*90/(pi/2)     #x,y,z>0 
+		lon=sgn(x[1])*acos(x[0]/((x[0]**2+x[1]**2)**0.5))*90./(pi/2)     #x,y,z>0 
 
 	#x axis is intersection of 0 longitude and 0 latitutde
 
@@ -33,55 +38,70 @@ def sgn(x):
 	return y
 
 def ecif2ecef(x,t):
-	
-	theta = w_earth*t #in radian
-	DCM = np.array([[cos(theta), sin(theta), 0], [-1*sin(theta), cos(theta),0], [0,0,1]])
+	#rotate vector in ecif to ecef
+	# time t in seconds
+	ut_sec = (LAUNCHDAY - EQUINOX).total_seconds() + t # universal time vector in sec 
+	theta = W_EARTH*ut_sec #in radian
+	DCM = np.array([[cos(theta), sin(theta), 0.], [-1*sin(theta), cos(theta),0.], [0.,0.,1.]])
 	y = np.dot(DCM,x)
 	
 	return y
 
 def ecif2ecefR(today,equinox,t):
 
-	ut_sec = (today - equinox).total_seconds() + t # universal time vector in sec
+	ut_sec = (LAUNCHDAY - EQUINOX).total_seconds() + t # universal time vector in sec
 	st_sec = steprut*ut_sec   #sidereal time vector in sec
 
-	phi = st_sec*w_earth           # sidereal time vector in rad
+	phi = st_sec*W_EARTH           # sidereal time vector in rad
 
-	TEI = np.array([[cos(phi),sin(phi),0],[-sin(phi),cos(phi),0],[0,0,1]])
+	TEI = np.array([[cos(phi),sin(phi),0.],[-sin(phi),cos(phi),0.],[0.,0.,1.]])
 
 	return TEI
 
 
 def ecef2ecif(x,t):
-
-	theta = w_earth*t #in radian
-	DCM = np.array([[cos(theta), -1*sin(theta), 0], [sin(theta), cos(theta),0],[ 0,0,1]])
+	#rotate vector from ecef to ecif
+	ut_sec = (LAUNCHDAY - EQUINOX).total_seconds() + t # universal time vector in sec
+	theta = W_EARTH*ut_sec #in radian
+	DCM = np.array([[cos(theta), -1*sin(theta), 0.], [sin(theta), cos(theta),0.],[ 0.,0.,1.]])
 	y = np.dot(DCM,x)
 	
 	return y
 
+
+
+def ecif2orbit(r,v,vector_ecif):
+	#r is position in eci frame
+	#v is velocity in eci frame
+    z = -r/np.linalg.norm(r)
+    y = np.cross(v,r)/np.linalg.norm(np.cross(v,r))
+    x = np.cross(y,z)
+    Rot_mat=np.array([x,y,z])
+    
+    u = np.dot(Rot_mat,vector_ecif)
+    
+    return u
+
+
+'''
+
 def ned2ecef(x,lat,lon):
+	#rotate vector from North-East-Down frame to ecef
+	# lat should be -90 to 90
+	# lon should be 0 to 360
 	v = np.array([-x[2], -x[0], x[1]]) #convert to spherical polar r theta phi
     
-	theta = -lat + 90 #in degree, polar angle
+	theta = -lat + 90. #in degree, polar angle (co-latitude)
 	phi = lon #in degree, azimuthal angle
 	theta = radians(theta)
 	phi = radians(phi)
 
 	DCM = np.array([[sin(theta)*cos(phi), cos(theta)*cos(phi), -sin(phi)],\
 		[sin(theta)*sin(phi), cos(theta)*sin(phi), cos(phi)],\
-		[cos(theta), -sin(theta), 0]]) #for spherical to cartesian
+		[cos(theta), -sin(theta), 0.]]) #for spherical to cartesian
 
 	y = np.dot(DCM,v)
 
 	return y
 
-def ecif2orbit(r,v,vector_ecif):
-    z=-r/np.linalg.norm(r)
-    y= np.cross(v,r)/np.linalg.norm(np.cross(v,r))
-    x=np.cross(y,z)
-    Rot_mat=np.array([x,y,z])
-    
-    u = np.dot(Rot_mat,vector_ecif)
-    
-    return u
+'''
