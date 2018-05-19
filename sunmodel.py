5b811fca7096441155894fb1a5be790d5f802104
Sanket Chirame <sanketchirame12@gmail.com>
2018-05-20 03:24:52 +0530

    Added QA-version and report of sun model

--- sunmodel.py
+++ sunmodel.py
@@ -1,28 +1,33 @@
 import numpy as np
-from datetime import *
 from constants_1U import LAUNCHDATE, EQUINOX
+import scipy.io as sio
+'''
+	This code gives sun-vector (unit vector from center of earth to center of sun) in ECI frame.
+	Input file is sgp data with first column as time series in seconds
+	The report for this code is uploaded on thread : Controls | Environment model
+'''
 
 
-sgp_output=np.genfromtxt('sgp_output.csv', delimiter=",")
-#initialtime = datetime(2018, 4, 03, 12, 50, 19) #Enter the starting time in year,month,day,hour,minute,second
-#equinox = datetime(2018, 3, 20, 13, 05, 00) #Enter the date and time for the closest equinox (Google this up)
-T = sgp_output[:,0]
+sgp_output = sio.loadmat('SGP_120k.mat')['SGP_120k'].transpose()
 
+T = sgp_output[:,0].copy()	#time in seconds
 N = len(T)
 si_output = np.zeros((N,4))
 
-initialdelay = (LAUNCHDATE - EQUINOX).days + (LAUNCHDATE - EQUINOX).seconds/86400.0 #Initial Delay in days
-
+#The time from equinox and the initial time (launch) in seconds
+initialdelay = (LAUNCHDATE - EQUINOX).total_seconds() 
 
 for i in range (N):
-	time = initialdelay + T[i] / 86400. #The time passed from equinox till each point in orbit in days
-	theta = (2*np.pi*time) / 365.256363 #Angle between intermediate frame (s) and (epsilon) frame about common z-axis
-	epsilon = 23.5 * np.pi / 180. #Angle between rotation axis and orbital plane normal
-	x=np.cos(theta)#components as got from document reffered
-	y=np.sin(theta)*np.cos(epsilon) 
-	z=np.sin(theta)*np.sin(epsilon) 
-	v_Sun = [x, y, z] #sun vector in ECI Frame
-	si_output[i,0] = T[i] #first component is time
-	si_output[i,1:4] = v_Sun;
-print np.shape(si_output)
-np.savetxt("si_output.csv", si_output, delimiter=",") #Saves sgp_output to csv file
+	time = (initialdelay + T[i]) / 86400. 	#The time passed from equinox till each point in orbit in days
+	theta = (2*np.pi*time) / 365.256363 	#Angle between intermediate frame (s) and (epsilon) frame about common z-axis
+	epsilon = 23.45 * np.pi / 180. 		#Angle between rotation axis and orbital plane normal
+	x = np.cos(theta)						#components as got from document reffered
+	y = np.sin(theta)*np.cos(epsilon) 
+	z = np.sin(theta)*np.sin(epsilon) 
+	v_sun_i = [x, y, z] 	#sun vector in ECI Frame
+	v_sun_i = v_sun_i/np.linalg.norm(v_sun_i.copy())
+	si_output[i,0] = T[i] 	#first component is time
+	si_output[i,1:4] = v_sun_i.copy();
+
+np.savetxt("si_output.csv", si_output, delimiter=",") #Saves si_output to csv file
+sio.savemat('si_output.mat', {'si_output':si_output})	#Saves si_output to mat file
