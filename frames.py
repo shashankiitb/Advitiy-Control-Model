5f3e1f8b87454a7063792ff2264080d78e662587
Sanket Chirame <sanketchirame12@gmail.com>
2018-06-03 22:20:57 +0530

    implemented naming convention in frame

--- frames.py
+++ frames.py
@@ -1,28 +1,30 @@
 #Code for transformation of vector from one reference frame to another
 
 import numpy as np
-from constants_1U import W_EARTH, LAUNCHDATE, EQUINOX, v_w_IOO
+from constants_1U import W_EARTH, LAUNCHDATE, EQUINOX, v_w_IO_o, STEPRUT
 from math import radians, sin, cos, acos, pi
 import qnv as qnv
 
-def latlon(x):
+def latlon(v_x):
 	#get the latitude and longitude in degrees given position in ECEF 
-	#x is position in ecif
+	#Input: x is position in ecef frame
+	#Output: latitude and longitude in degres (tuple)
 	#latitude ranges [0,90] in north hemisphere and [0,-90] in south hemisphere
-	lat=sgn(x[2])*(acos(((x[0]**2+x[1]**2)**0.5)/((x[0]**2+x[1]**2+x[2]**2)**0.5)))*90./(pi/2.) 
+
+	lat = sgn(v_x[2])*(acos(((v_x[0]**2 + v_x[1]**2)**0.5)/((v_x[0]**2 + v_x[1]**2 + v_x[2]**2)**0.5)))*90./(pi/2.) 
 	   
 	# longitude calculation given position, lon is longitude
 	#ranges from (-pi,pi]
-	if x[1]==0:
-		if x[0]>=0 :
+	if v_x[1]==0:
+		if v_x[0]>=0 :
 			lon = 0.0
 		else:
 			lon = 180.0
 
 	else:
-		lon=sgn(x[1])*acos(x[0]/((x[0]**2+x[1]**2)**0.5))*90./(pi/2)     #x,y,z>0 
+		lon = sgn(v_x[1])*acos(v_x[0]/((v_x[0]**2 + v_x[1]**2)**0.5))*90./(pi/2)      
 
-	#x axis is intersection of 0 longitude and 0 latitutde
+	#x axis is intersection of 0 longitude and 0 latitude
 
 	return lat,lon
 
@@ -37,20 +39,20 @@ def sgn(x):
 
 	return y
 
-def ecif2ecef(x,t):
-	#Input ecif cector and time since launch in seconds
+def ecif2ecef(v_x_i,t):
+	#Input ecif cector and time since epoch in seconds
 	# output ecef vector
 	ut_sec = (LAUNCHDATE - EQUINOX).total_seconds() + t # universal time vector in sec 
 	theta = W_EARTH*ut_sec #in radian
-	DCM = np.array([[cos(theta), sin(theta), 0.], [-1*sin(theta), cos(theta),0.], [0.,0.,1.]])
-	y = np.dot(DCM,x)
+	m_DCM = np.array([[cos(theta), sin(theta), 0.], [-1*sin(theta), cos(theta),0.], [0.,0.,1.]])
+	v_x_e = np.dot(m_DCM,v_x_i)
 	
-	return y
+	return v_x_e
 
-def ecif2ecefR(today,equinox,t):
+def ecif2ecefR(t):
 
 	ut_sec = (LAUNCHDATE - EQUINOX).total_seconds() + t # universal time vector in sec
-	st_sec = steprut*ut_sec   #sidereal time vector in sec
+	st_sec = STEPRUT*ut_sec   #sidereal time vector in sec
 
 	phi = st_sec*W_EARTH           # sidereal time vector in rad
 
@@ -59,80 +61,82 @@ def ecif2ecefR(today,equinox,t):
 	return TEI
 
 
-def ecef2ecif(x,t):
-	#Input ecef cector and time since launch in seconds
+def ecef2ecif(v_x_e,t):
+	#Input ecef cector and time since epoch in seconds
 	# output ecif vector
 	ut_sec = (LAUNCHDATE - EQUINOX).total_seconds() + t # universal time vector in sec
 	theta = W_EARTH*ut_sec #in radian
-	DCM = np.array([[cos(theta), -1*sin(theta), 0.], [sin(theta), cos(theta),0.],[ 0.,0.,1.]])
-	y = np.dot(DCM,x)
+	m_DCM = np.array([[cos(theta), -1*sin(theta), 0.], [sin(theta), cos(theta),0.],[ 0.,0.,1.]])
+	v_x_i = np.dot(m_DCM,v_x_e)
 	
-	return y
+	return v_x_i
 
 
 
-def ecif2orbit(r,v,vector_ecif):
-	#r is position in eci frame
-	#v is velocity in eci frame
-	z = -r/np.linalg.norm(r)
-	y = np.cross(v,r)/np.linalg.norm(np.cross(v,r))
-	x = np.cross(y,z)
-	Rot_mat=np.array([x,y,z])
+def ecif2orbit(v_pos_i,v_vel_i,v_x_i):
+	#Input: v_pos_i is position in eci frame , v_vel_i is velocity in eci frame, v_x_i is vector to be transformed
+	#output: vector components in orbit frame
+	z = -v_pos_i/np.linalg.norm(v_pos_i)
+	y = np.cross(v_vel_i,v_pos_i)/np.linalg.norm(np.cross(v_vel_i,v_pos_i))
+	x = np.cross(y,z)/np.linalg.norm(np.cross(y,z))
+	m_DCM_OI = np.array([x,y,z])
 	
-	u = np.dot(Rot_mat,vector_ecif)
+	v_x_o = np.dot(m_DCM_OI,v_x_i)
 	
-	return u
+	return v_x_o
 
-def qBI_2_qBO(qBI,r,v):
-	#input: quaternion which rotates ecif vector to body frame
-	#output: quaternion which rotates orbit frame vector to body frame
+def qBI2qBO(v_q_BI,v_pos_i,v_vel_i):
+	#input: unit quaternion which rotates ecif vector to body frame, position and velocity in ecif
+	#output: unit quaternion which rotates orbit frame vector to body frame
 	#Orbit frame def:	z_o = nadir(opposite to satellite position vector) y_o: cross(v,r) x_o: cross(y,z) 
 
-	z = -r/np.linalg.norm(r)
-	y = np.cross(v,r)/np.linalg.norm(np.cross(v,r))
+	z = -v_pos_i/np.linalg.norm(v_pos_i)
+	y = np.cross(v_vel_i,v_pos_i)/np.linalg.norm(np.cross(v_vel_i,v_pos_i))
 	x = np.cross(y,z)/np.linalg.norm(np.cross(y,z))
-	M_OI = np.array([x,y,z])
+	m_DCM_OI = np.array([x,y,z])
 
-	qIO = qnv.rotm2quat(np.transpose(M_OI))
+	v_q_IO = qnv.rotm2quat(np.transpose(m_DCM_OI))
 
-	G1 = np.array([[ qBI[0],	-qBI[1], -qBI[2], -qBI[3]],
-				  [	qBI[1],	qBI[0],	qBI[3],	-qBI[2]],
-				  [ qBI[2], -qBI[3], qBI[0], qBI[1]],
-				  [ qBI[3], qBI[2], -qBI[1], qBI[0]]])
+	m_G1 = np.array([[ v_q_BI[0],	-v_q_BI[1], -v_q_BI[2], -v_q_BI[3]],
+				  [	v_q_BI[1],	v_q_BI[0],	v_q_BI[3],	-v_q_BI[2]],
+				  [ v_q_BI[2], -v_q_BI[3], v_q_BI[0], v_q_BI[1]],
+				  [ v_q_BI[3], v_q_BI[2], -v_q_BI[1], v_q_BI[0]]])
 
-	qBO = np.dot(G1,qIO)
+	v_q_BO = np.dot(m_G1,v_q_IO)
 
-	if qBO[0] < 0.:
-		qBO = -1.*qBO.copy()
+	if v_q_BO[0] < 0.:
+		v_q_BO = -1.*v_q_BO.copy()
+	v_q_BO = v_q_BO/np.linalg.norm(v_q_BO.copy())	
 
-	return qBO
+	return v_q_BO
 
-def qBO_2_qBI(qBO,r,v):
-	#input: quaternion which rotates orbit frame vector to body frame
-	#output: quaternion which rotates ecif vector to body frame
+def qBO2qBI(v_q_BO,v_pos_i,v_vel_i):
+	#input: Unit quaternion which rotates orbit frame vector to body frame, position and velocity in ecif
+	#output: Unit quaternion which rotates ecif vector to body frame
 	#Orbit frame def:	z_o = nadir(opposite to satellite position vector) y_o: cross(v,r) x_o: cross(y,z)
-	z = -r/np.linalg.norm(r)
-	y = np.cross(v,r)/np.linalg.norm(np.cross(v,r))
+	z = -v_pos_i/np.linalg.norm(v_pos_i)
+	y = np.cross(v_vel_i,v_pos_i)/np.linalg.norm(np.cross(v_vel_i,v_pos_i))
 	x = np.cross(y,z)/np.linalg.norm(np.cross(y,z))
-	M_OI = np.array([x,y,z])
+	m_DCM_OI = np.array([x,y,z])
 
-	qOI = qnv.rotm2quat(M_OI)
-	G1 = np.array([[ qBO[0],	-qBO[1], -qBO[2], -qBO[3]],
-				  [	qBO[1],	qBO[0],	qBO[3],	-qBO[2]],
-				  [ qBO[2], -qBO[3], qBO[0], qBO[1]],
-				  [ qBO[3], qBO[2], -qBO[1], qBO[0]]])
-	qBI = np.dot(G1,qOI)
+	v_q_OI = qnv.rotm2quat(m_DCM_OI)
+	m_G1 = np.array([[ v_q_BO[0],	-v_q_BO[1], -v_q_BO[2], -v_q_BO[3]],
+				  [	v_q_BO[1],	v_q_BO[0],	v_q_BO[3],	-v_q_BO[2]],
+				  [ v_q_BO[2], -v_q_BO[3], v_q_BO[0], v_q_BO[1]],
+				  [ v_q_BO[3], v_q_BO[2], -v_q_BO[1], v_q_BO[0]]])
+	v_q_BI = np.dot(G1,v_q_OI)
 
-	if qBI[0] < 0. :
-		qBI = -1.*qBI.copy()
+	if v_q_BI[0] < 0. :
+		v_q_BI = -1.*v_q_BI.copy()
+	v_qBI = v_q_BI/np.linalg.norm(v_q_BI.copy())	
 
-	return qBI
+	return v_q_BI
 
-def wBIB_2_wBOB(wBIB,qBO):
-	#input: angular rate of body wrt ecif in body frame, quaternion which rotates orbit vector to body frame
-	#output: angular rate of body frame wrt orbit frame in body frame
+def wBIb2wBOb(v_w_BI_b,v_q_BO):
+	#input: angular velocity of body wrt ecif in body frame, unit quaternion which rotates orbit vector to body frame
+	#output: angular velocity of body frame wrt orbit frame in body frame
 	
-	return wBIB + qnv.quatRotate(qBO,v_w_IOO)
+	return v_w_BI_b + qnv.quatRotate(v_q_BO,v_w_IO_o)
 
 
 
