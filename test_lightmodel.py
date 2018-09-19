b736b11c3db7a5bf8066b5e96b659a1ddae798e0
Shashank Verma <18856352+shashankiitb@users.noreply.github.com>
2018-09-19 15:04:40 +0530

    updated file

--- test_lightmodel.py
+++ test_lightmodel.py
@@ -3,17 +3,17 @@ import unittest	#testing library
 import numpy as np
 from ddt import ddt,file_data,unpack,data
 
-m_test_lightmodel_output = np.genfromtxt(r'E:\\Student Satellite\\Control Stuff and Feb 2018 onwards\\CLS\\Advitiy-Control-Model-detumbling\\light_output.csv', delimiter=',')
+m_test_lightmodel_output = np.genfromtxt(r'D:\Shashank\Advitiy-Control-Model-detumbling\light_output.csv', delimiter=',')
 
 @ddt
 class TestAdd(unittest.TestCase):
-	@file_data("test-data/test_add-Copy.json")	#File can contain nested list 
+	@file_data("test-data/test_lightmodel.json")	#File can contain nested list 
 	#first and second number in each sub-list are numbers to be added and third number is expected result
 	def test_cases_file(self,value):
 		time = value[0]
 		expected = value[1]
-		result = m_test_lightmodel_output[t*10]
-		
+		result = m_test_lightmodel_output[time*10,1]
+		print expected, result
 		self.assertTrue(np.allclose(result,expected))	
 		
 
