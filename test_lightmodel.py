68adc34e4a54584591a9124491ffcfd673eadf2d
Shashank Verma <18856352+shashankiitb@users.noreply.github.com>
2018-09-22 13:46:47 +0530

    Files related to lightmodel QA

--- test_lightmodel.py
+++ test_lightmodel.py
@@ -3,7 +3,7 @@ import unittest	#testing library
 import numpy as np
 from ddt import ddt,file_data,unpack,data
 
-m_test_lightmodel_output = np.genfromtxt(r'D:\Shashank\Advitiy-Control-Model-detumbling\light_output.csv', delimiter=',')
+m_test_lightmodel_output = np.genfromtxt(r'E:\Student Satellite\Control Stuff and Feb 2018 onwards\CLS\Advitiy-Control-Model-detumbling\light_output.csv', delimiter=',')
 
 @ddt
 class TestAdd(unittest.TestCase):
@@ -12,7 +12,7 @@ class TestAdd(unittest.TestCase):
 	def test_cases_file(self,value):
 		time = value[0]
 		expected = value[1]
-		result = m_test_lightmodel_output[time*10,1]
+		result = m_test_lightmodel_output[time*10,1] #Time interval is 0.1sec, so index is time*10 
 		print expected, result
 		self.assertTrue(np.allclose(result,expected))	
 		
